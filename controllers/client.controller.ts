4e49049a974ca38f96181303d04282a69b01007d
angel0las0 <angelopolis1_2009@hotmail.com>
2024-10-25 19:19:47 -0600

    fix: minor details added for swagger configuration y readme file

--- controllers/client.controller.ts
+++ controllers/client.controller.ts
@@ -6,61 +6,66 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 interface Client {
-    name: string;
-    lastName: string;
-    phone: string;
-    invoice: boolean;
-    socialReazon?: string;
-    zipcode?: string;
-    fiscalRegimen?: string;
-    email?: string;
+	name: string
+	lastName: string
+	phone: string
+	invoice: boolean
+	socialReazon?: string
+	zipcode?: string
+	fiscalRegimen?: string
+	email?: string
 }
 
 export const newClient = async (req: Request, res: Response) => {
-    const { name, lastName, phone, invoice, socialReazon, zipcode, fiscalRegimen, email } = req.body;
+	const { name, lastName, phone, invoice, socialReazon, zipcode, fiscalRegimen, email } = req.body
 
-    let responseStatus = StatusCodes.CREATED;
-    let responseContents;
+	let responseStatus = StatusCodes.CREATED
+	let responseContents
 
-    // Validación de campos obligatorios
-    if (!name || !lastName || !phone || invoice === undefined) {
-        responseStatus = StatusCodes.BAD_REQUEST;
-        responseContents = { error: 'name, lastName, phone and invoice (true or false) fields are required' };
-        return res.status(responseStatus).send(responseContents);
-    }
+	// Validación de campos obligatorios
+	if (!name || !lastName || !phone || invoice === undefined) {
+		responseStatus = StatusCodes.BAD_REQUEST
+		responseContents = { error: 'name, lastName, phone and invoice (true or false) fields are required' }
+		return res.status(responseStatus).send(responseContents)
+	}
 
-    const newClient: Client = {
-        name,
-        lastName,
-        phone,
-        invoice,
-        socialReazon: invoice ? socialReazon : null,
-        zipcode: invoice ? zipcode : null,
-        fiscalRegimen: invoice ? fiscalRegimen : null,
-        email: invoice ? email : null,
-    };
+	const newClient: Client = {
+		name,
+		lastName,
+		phone,
+		invoice,
+		socialReazon: invoice ? socialReazon : null,
+		zipcode: invoice ? zipcode : null,
+		fiscalRegimen: invoice ? fiscalRegimen : null,
+		email: invoice ? email : null,
+	}
 
-    try {
-    		await prisma.client.create({
-            	data: {
-                	name: newClient.name,
-                	lastName: newClient.lastName,
-                	phone: newClient.phone,
-                	invoice: newClient.invoice,
-					socialReazon: newClient.socialReazon,
-					zipcode: newClient.zipcode,
-					fiscalRegimen: newClient.fiscalRegimen,
-					email: newClient.email
-            	},
-        	});
+	try {
+		await prisma.client.create({
+			data: {
+				name: newClient.name,
+				lastName: newClient.lastName,
+				phone: newClient.phone,
+				invoice: newClient.invoice,
+				socialReazon: newClient.socialReazon,
+				zipcode: newClient.zipcode,
+				fiscalRegimen: newClient.fiscalRegimen,
+				email: newClient.email,
+			},
+		})
 
-        responseContents = { message: 'Client Registered successfully' };
-    } catch (error) {
-        console.error(`[POST] clientController/newClient error: ${error}`);
-        responseStatus = StatusCodes.INTERNAL_SERVER_ERROR;
-        responseContents = { error: `Internal server error: ${error}` };
-        return res.status(responseStatus).send(responseContents);
-    }
+		responseContents = { message: 'Client Registered successfully' }
+	} catch (error) {
+		console.error(`[POST] clientController/newClient error: ${error}`)
+		responseStatus = StatusCodes.INTERNAL_SERVER_ERROR
+		responseContents = { error: `Internal server error: ${error}` }
+		return res.status(responseStatus).send(responseContents)
+	}
 
-    return res.status(responseStatus).send(responseContents);
-};
+	return res.status(responseStatus).send(responseContents)
+}
+
+export const updateClientInvoiceDetails = async (req: Request, res: Response) => {}
+export const updateClientDetails = async (req: Request, res: Response) => {}
+export const getClientDetails = async (req: Request, res: Response) => {}
+export const getAllClients = async (req: Request, res: Response) => {}
