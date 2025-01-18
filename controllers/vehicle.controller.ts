c08d7716ae4bd0a16ebb0bb8d5af2a95832f5222
angel0las0 <angelopolis1_2009@hotmail.com>
2025-01-17 23:23:15 -0600

    incomplete feature

--- controllers/vehicle.controller.ts
+++ controllers/vehicle.controller.ts
@@ -2,7 +2,6 @@ import { Request, response, Response } from 'express'
 import prisma from '../prisma/prisma'
 import { StatusCodes } from 'http-status-codes'
 import * as dotenv from 'dotenv'
-import { verify } from 'crypto'
 dotenv.config()
 
 interface Vehicle {
@@ -75,3 +74,85 @@ export const newVehicle = async (req: Request, res: Response) => {
 
 	return res.status(responseStatus).send(responseContents)
 }
+
+export const getVehicle = async (req: Request, res: Response) => {
+	const { idVehicle } = req.body
+	let responseStatus = StatusCodes.OK
+	let responseContents
+
+	try {
+		if (!idVehicle) {
+			responseStatus = StatusCodes.BAD_REQUEST
+			responseContents = { error: 'idVehicle field required' }
+			return res.status(responseStatus).send(responseContents)
+		}
+
+		const vehicle = await prisma.vehicle.findFirst({ where: { idVehicle: idVehicle } })
+
+		if (!vehicle) {
+			responseStatus = StatusCodes.NOT_FOUND
+			responseContents = { error: `No vehicle found with id: ${idVehicle}` }
+			return res.status(responseStatus).send(responseContents)
+		}
+
+		responseContents = vehicle
+	} catch (error) {
+		console.error(`[GET] vehicleController/getVehicle error: ${error}`)
+		responseStatus = StatusCodes.INTERNAL_SERVER_ERROR
+		responseContents = { error: `[GET] vehicleController/getVehicle. Internal server error: ${error}` }
+		return res.status(responseStatus).send(responseContents)
+	}
+
+	return res.status(responseStatus).send(responseContents)
+}
+export const getAllVehiclesFromclient = async (req: Request, res: Response) => {
+	const { idClient } = req.body
+	let responseStatus = StatusCodes.OK
+	let responseContents
+
+	try {
+		if (!idClient) {
+			responseStatus = StatusCodes.BAD_REQUEST
+			responseContents = { error: 'idClient field required' }
+			return res.status(responseStatus).send(responseContents)
+		}
+
+		const vehicles = await prisma.vehicle.findMany({ where: { cliente_idcliente: idClient } })
+
+		if (!vehicles) {
+			responseStatus = StatusCodes.NOT_FOUND
+			responseContents = { error: `Vehicles owned by client id ${idClient} not found` }
+			return res.status(responseStatus).send(responseContents)
+		}
+
+		responseContents = vehicles
+	} catch (error) {
+		console.error(`[GET] vehicleController/getAllVehiclesFromClient error: ${error}`)
+		responseStatus = StatusCodes.INTERNAL_SERVER_ERROR
+		responseContents = { error: `[GET] vehicleController/getAllVehiclesFromClient. Internal server error: ${error}` }
+		return res.status(responseStatus).send(responseContents)
+	}
+
+	return res.status(responseStatus).send(responseContents)
+}
+//SE NECESITA TERMINAR ESTE CONTROLLER Y ADEMAS LOS METODOS DE UPDATEVEHICLE Y DELETEVEHICLE
+export const updateVehicle = async (req: Request, res: Response) => {
+	const { idVehicle } = req.body
+    let responseStatus = StatusCodes.OK
+    let responseContents
+
+    try {
+        if(!idVehicle){
+            responseStatus = StatusCodes.BAD_REQUEST
+			responseContents = { error: 'idVehicle field required' }
+			return res.status(responseStatus).send(responseContents)
+        }
+       
+
+
+    } catch (error) {
+        
+    }
+}
+export const deteleVehicle = async (req: Request, res: Response) => {}
+
