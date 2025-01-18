20b935ab79fd84fa0d3c2702b310247a33ebeab5
angel0las0 <angelopolis1_2009@hotmail.com>
2025-01-18 00:50:16 -0600

    refactor: - auth.controller/registerUser refactored and logger added - passport implementation for future routing protection - express.d.ts data types for request extra fields removed due to passport and userInfor.ts implementation - swagger.yml removed since is not necesary - unnecesary controllers removed

--- controllers/reception/vehicle.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express'
-import { JwtPayloadExtended } from '../../express'
-import prisma from '../../prisma/prisma'
-import { StatusCodes } from 'http-status-codes'
-
-export const registerVehicle = async (req: Request, res: Response) => {
-    
-}
